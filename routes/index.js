5b15b4cb3695f025b7cf1a1e1a04aeef24e1068c
sanjay <sanjayk2310btechcse2023@kccitm.edu.in>
2022-05-31 16:40:54 +0530

    utpper

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req)
+  res.json({name:"utrkisht"})
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
