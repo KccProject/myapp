4ac894015d82d87fd186a113f3376835e5e286e0
sanjay <sanjayk2310btechcse2023@kccitm.edu.in>
2022-06-15 16:52:14 +0530

    RE Update

--- routes/login.js
+++ routes/login.js
@@ -12,7 +12,7 @@ var con = mysql.createConnection({
   database: "kccStudent"
 });
 
-
+con.connect()
 
 router.get('/', function (req, res, next) {
   console.log("Nitya");
@@ -29,22 +29,31 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/getLogin', function (req, res, next) {
-  console.log(req.query)
-  con.connect()
+
+
   con.query("select * from form", function (err, result) {
     if (err) throw err;
     console.log(result);
     res.render('login', { "data": result })
   });
-  // res.send({"name":"himani"})
-  //res.render('teacher')
-  // res.json({ "Name": "Idiot" })
+
 });
 
 router.post('/updateLogin', function (req, res, next) {
   console.log(req.body)
   res.json({ "Name": "Hello" })
 });
+
+router.post('/deleteLogin', function (req, res, next) {
+  console.log(req.body.id)
+  // res.json({ "Name": "Hello" })
+  con.query("DELETE FROM form where id ="+req.body.id, function (err, result) {
+    if (err) res.json({code:0});
+    console.log(result);
+    res.json({code:1})
+  });
+});
+
 router.post('/getLogin', function (req, res, next) {
   getLoginPost(req, res, next)
   // console.log(req.body)
